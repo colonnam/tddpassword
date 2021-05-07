15705e1793353963d93c0ef9a35b256dea5f9b10
martin <martin@localhost>
2021-05-07 13:51:58 +0200

    start

---
+++ src/main/java/tddpassword/start/Start.java
@@ -0,0 +1,8 @@
+package tddpassword.start;
+
+public class Start {
+
+	public static int R1() {
+		return 1; 
+	}
+}
---
+++ src/test/java/tddpassword/start/Teststart.java
@@ -0,0 +1,13 @@
+package tddpassword.start;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Teststart {
+
+	@Test
+	public void testr1() {
+		Assertions.assertEquals(1,Start.R1());
+	}
+	
+}
